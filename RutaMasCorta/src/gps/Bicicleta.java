9a46fdd5f9d6086991addd2f862ea40df6c0fb78
Álvaro Rodríguez <alvaro.rodriguez.martinez98@gmail.com>
2020-02-03 09:37:23 +0100

    -Solucion añadiendo for para cada camino

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -22,10 +22,14 @@ public class Bicicleta {
         for(int i=0; i<camino1.length; i++)
         {
             sum1 += camino1[i];
-            sum2 += camino2[i];
-            sum3 += camino3[i];
         }
-        
+        for(int i=0; i<camino2.length; i++)
+        {
+            sum2 += camino2[i];
+        }
+        for(int i=0; i<camino3.length; i++)
+        {            sum3 += camino3[i];
+        }
         if(sum1 < sum2 && sum1 < sum3)
         {
             return "camino 1"; 
@@ -55,7 +59,7 @@ public class Bicicleta {
         Bicicleta bici = new Bicicleta();
         int[] camino1 = {12,34,21,46,25};
         int[] camino2 = {24,1,5,64,10,15,21};
-        int[] camino3 = {1,5,6,7,8,10,20,46,104};
+        int[] camino3 = {1,5,6,7,8};
         System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
     }
 }
